de1bc805ac2c90d3648413813c12b5ecd8d3c574
Afanas2707 <avg57327@gmail.com>
2023-10-05 23:25:21 +0300

    first123

--- Homework/main.py
+++ Homework/main.py
@@ -4,6 +4,7 @@ from random import randrange
 from xml.etree import ElementTree
 import names
 
+
 class Person:
     def __init__(self, name: str, age: int):
         self.name = name
@@ -76,16 +77,13 @@ class MathCourse:
 
     @classmethod
     def from_json(cls, json_data):
-        # Преобразование JSON данных в объект MathCourse
+
         course_dict = json.loads(json_data)
         course_code = course_dict["course_code"]
         course_name = course_dict["course_name"]
 
-        # Создание объекта MathCourse
         math_course = cls(course_code, course_name)
 
-        # Если в JSON данных также содержится информация о профессоре,
-        # создаем объект Professor и присваиваем его атрибуту professor
         professor_data = course_dict.get("professor", None)
         if professor_data:
             professor = Professor(
@@ -96,8 +94,6 @@ class MathCourse:
             )
             math_course.professor = professor
 
-        # Если в JSON данных также содержится информация о студентах,
-        # создаем объекты Student и добавляем их к атрибуту students
         students_data = course_dict.get("students", [])
         for student_data in students_data:
             student = Student(
@@ -111,14 +107,12 @@ class MathCourse:
         return math_course
 
     def to_xml(self):
-        # Создаем корневой элемент <MathCourse>
+
         math_course_element = ElementTree.Element("MathCourse")
 
-        # Добавляем атрибуты course_code и course_name
         math_course_element.set("course_code", str(self.course_code))
         math_course_element.set("course_name", self.course_name)
 
-        # Если есть информация о профессоре, создаем элемент <Professor>
         if self.professor:
             professor_element = ElementTree.Element("Professor")
             professor_element.set("salary", str(self.professor.salary))
@@ -127,7 +121,6 @@ class MathCourse:
             professor_element.set("degree", self.professor.degree)
             math_course_element.append(professor_element)
 
-        # Если есть информация о студентах, создаем элемент <Students>
         if self.students:
             students_element = ElementTree.Element("Students")
             for student in self.students:
@@ -139,26 +132,21 @@ class MathCourse:
                 students_element.append(student_element)
             math_course_element.append(students_element)
 
-        # Создаем XML-дерево
         xml_tree = ElementTree.ElementTree(math_course_element)
 
-        # Возвращаем XML в виде строки
         return ElementTree.tostring(xml_tree.getroot(), encoding="utf-8", method="xml")
 
     @classmethod
     def from_xml(cls, xml_data):
-        # Разбор XML данных
+
         xml_tree = ElementTree.ElementTree(ElementTree.fromstring(xml_data))
         math_course_element = xml_tree.getroot()
 
-        # Извлечение атрибутов course_code и course_name
         course_code = int(math_course_element.get("course_code"))
         course_name = math_course_element.get("course_name")
 
-        # Создание объекта MathCourse
         math_course = cls(course_code, course_name)
 
-        # Извлечение информации о профессоре, если она есть
         professor_element = math_course_element.find("Professor")
         if professor_element is not None:
             salary = int(professor_element.get("salary"))
@@ -167,7 +155,6 @@ class MathCourse:
             degree = professor_element.get("degree")
             math_course.professor = Professor(salary, name, age, degree)
 
-        # Извлечение информации о студентах, если она есть
         students_element = math_course_element.find("Students")
         if students_element is not None:
             for student_element in students_element.findall("Student"):
@@ -201,7 +188,8 @@ hobby_list = ["Education", "Fashion", "Fitness", "Music", "Nature", "Playing"]
 
 for i in range(0, 999):
     try:
-        math_course.enroll_student(Student(hobby=hobby_list[randrange(len(hobby_list))], name=names.get_first_name(), age=random.randint(18, 27), grade=chr(random.randint(ord('A'), ord('Z')))))
+        math_course.enroll_student(Student(hobby=hobby_list[randrange(len(hobby_list))], name=names.get_first_name(),
+                                           age=random.randint(18, 27), grade=chr(random.randint(ord('A'), ord('Z')))))
     except TooManyStudentsEnrolledException:
         print("Custom exception handled")
         break
@@ -220,7 +208,6 @@ with open('math_course_from_json.json', 'r') as json_file:
     json_data = json_file.read()
 
 math_course_from_json = MathCourse.from_json(json_data)
-
 
 xml_data = math_course.to_xml()
 
